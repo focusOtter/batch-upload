98d75eb4a1c196f91c122d2fc97fc5acc2d9e05c
Michael Liendo <mtliendo@gmail.com>
2022-06-07 00:04:27 -0500

    csvToJSon final

--- src/routes/AdminPage.js
+++ src/routes/AdminPage.js
@@ -1,10 +1,27 @@
 import { Button, TextField } from '@aws-amplify/ui-react'
-import React from 'react'
+import React, { useState } from 'react'
+import Papa from 'papaparse'
 
 function AdminPage() {
-	const handleCSVSubmit = () => {}
-	const handleCSVUpload = () => {}
+	const [userJSON, setUserJSON] = useState()
+	const [uploadedCSV, setUploadedCSV] = useState(null)
 
+	const handleCSVSubmit = (e) => {
+		e.preventDefault()
+
+		Papa.parse(uploadedCSV, {
+			header: true,
+			complete: (results) => {
+				console.log({ results })
+				setUserJSON(results)
+			},
+		})
+	}
+
+	const handleCSVUpload = (e) => {
+		console.log(e.target.files[0])
+		setUploadedCSV(e.target.files[0])
+	}
 	return (
 		<form onSubmit={handleCSVSubmit}>
 			<TextField
